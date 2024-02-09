acbf541d3d932b2c104afae12eafcda6342fed0a
zeusXmachina <116007591+zeusXmachina@users.noreply.github.com>
2024-02-09 23:46:45 +0000

    logger and level editor start

---
+++ Assets/Code/Development/ZXMLogger.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+namespace ZXM.Development
+{
+    public class ZXMLogger : MonoBehaviour
+    {
+        public static ZXMLogger Instance;
+        [Tooltip("Use to Toggle Debugs")]
+        [SerializeField]private bool isLogging = true;
+        public bool IsLogging
+        {
+            get { return isLogging; }
+            set { isLogging = value; }
+        }
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+            //persistent
+            DontDestroyOnLoad(gameObject);
+        }
+        /// <summary>
+        /// test logger 
+        /// </summary>
+        /// <param name="message"></param>
+        public void Log(string message)
+        {
+            if (isLogging)
+            {
+                Debug.Log(message);
+            }
+        }   
+
+    }
+}
