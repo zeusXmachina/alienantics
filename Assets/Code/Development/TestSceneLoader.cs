7a68d76f2b7f3e0d285b4b6cfc4a51b9dc1be046
zeusXmachina <116007591+zeusXmachina@users.noreply.github.com>
2024-03-04 23:43:41 +0000

    testing scene wip 2

--- Assets/Code/Development/TestSceneLoader.cs
+++ Assets/Code/Development/TestSceneLoader.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using ZXM.World;
+using System.IO;
 
 namespace ZXM.Development
 {
@@ -11,6 +13,29 @@ namespace ZXM.Development
     /// </summary>
     public class TestSceneLoader : MonoBehaviour
     {
-
+        [SerializeField] private BlockPlacer cubePlacer;
+        [SerializeField] private BlockData blockData;
+        private void Start()
+        {
+            var x = Resources.Load<TextAsset>("testData").ToString();
+            LoadBlocks(x);
+        }
+        /// <summary>
+        /// testing loading from json
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public void LoadBlocks(string data)
+        {
+            //if (data == null)
+            //{
+            //    data = saveData;
+            //}
+            blockData = JsonUtility.FromJson<BlockData>(data);
+            foreach (var block in blockData.blocks)
+            {
+                cubePlacer.PlaceCubeNear(new Vector3(block.positionX, block.positionY, block.positionZ));
+            }
+        }
     }
 }
