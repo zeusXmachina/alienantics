494840e362bd2e070d2828a3c1ccd679d7e35220
zeusXmachina <116007591+zeusXmachina@users.noreply.github.com>
2024-03-04 01:17:06 +0000

    testing scene wip 1

---
+++ Assets/Code/Development/TestSceneLoader.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace ZXM.Development
+{
+    /// <summary>
+    /// test loading a scene from stored json data
+    /// check what is required and update the json data accordingly
+    /// add a basic player 
+    /// add a basic camera
+    /// add a basic movement script
+    /// </summary>
+    public class TestSceneLoader : MonoBehaviour
+    {
+
+    }
+}
